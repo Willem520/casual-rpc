04d42970fa39af793bbced163716ac7b139f56d4
weiyu <weiyu005@ke.com>
2019-04-23 15:55:41 +0800

    初始化项目

---
+++ casual-core/src/main/java/willem/weiyu/casual/common/CasualRequest.java
@@ -0,0 +1,32 @@
+package willem.weiyu.casual.common;
+
+import lombok.Data;
+
+/**
+ * @Author weiyu
+ * @Description
+ * @Date 2019/4/23 15:52
+ */
+@Data
+public class CasualRequest {
+    /**
+     * 调用编号
+     */
+    private String requestId;
+    /**
+     * 类名
+     */
+    private String className;
+    /**
+     * 方法名
+     */
+    private String methodName;
+    /**
+     * 请求参数类型
+     */
+    private Class<?>[] parameterType;
+    /**
+     * 请求参数
+     */
+    private Object[] parameters;
+}
---
+++ casual-core/src/main/java/willem/weiyu/casual/common/CasualResponse.java
@@ -0,0 +1,12 @@
+package willem.weiyu.casual.common;
+
+import lombok.Data;
+
+/**
+ * @Author weiyu
+ * @Description
+ * @Date 2019/4/23 15:55
+ */
+@Data
+public class CasualResponse {
+}
